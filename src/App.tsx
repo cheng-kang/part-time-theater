d5a8b1bfe0d37e48fc3be89b15c8980231e6ca9a
程康 <me@chengkang.pw>
2021-10-03 13:06:24 -0700

    init

---
+++ src/App.tsx
@@ -0,0 +1,15 @@
+import React from 'react';
+import './App.css';
+
+function App() {
+  return (
+    <div className="container">
+      <h1 data-shadow='Part-time Theater'>Part-time Theater</h1>
+      <br />
+      <br />
+      <h3>Opening soon.</h3>
+    </div>
+  );
+}
+
+export default App;
