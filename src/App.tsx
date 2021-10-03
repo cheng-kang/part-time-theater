61bff0fb60e5827740ea2d5a68c265abfbeea3f9
程康 <me@chengkang.pw>
2021-10-03 16:46:40 -0700

    feat: better layout

--- src/App.tsx
+++ src/App.tsx
@@ -5,6 +5,7 @@ import List from "./List";
 function App() {
   return (
     <div className="container">
+      <main>
       <h1 data-shadow="Part-time Theater">Part-time Theater</h1>
       <br />
       <br />
@@ -14,6 +15,8 @@ function App() {
       <div className="list-container">
         <List />
       </div>
+      </main>
+      <footer>©️2021 CHENGKANG. All rights reserved.</footer>
     </div>
   );
 }
